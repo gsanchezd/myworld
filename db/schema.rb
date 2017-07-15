83b21bea868f689afbf45cf009934ac2acd72c02
Gonzalo <gonzalo.sanchez.d@gmail.com>
2017-07-15 17:52:55 -0400

    add name and profile to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715210435) do
+ActiveRecord::Schema.define(version: 20170715215039) do
+
+  create_table "add_name_and_profile_to_users", force: :cascade do |t|
+    t.string "name"
+    t.text "profile"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -48,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170715210435) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
