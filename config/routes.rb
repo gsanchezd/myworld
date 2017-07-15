9331b779b3ea39581d57d31d40a5be1f97a03ce6
David Barrientos <david.barrientos.f@gmail.com>
2017-07-15 18:40:27 -0400

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
+
   resources :jobs do
     member do
       get 'apply'
     end
   end
 
-  Rails.application.routes.draw do
-    devise_for :users, controllers: {
-      registrations: 'users/registrations'
-    }
-  end
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root to: 'jobs#index'
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
