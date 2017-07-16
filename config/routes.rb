b1e78e41b2fbf5d1cdf0beb49f8d57bfd31c1ada
David Barrientos <david.barrientos.f@gmail.com>
2017-07-16 14:39:07 -0400

    root path changed

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  root to: 'jobs#index'
+  root to: 'landings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
